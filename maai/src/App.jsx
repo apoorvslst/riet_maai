3f201680f3ec240a6e788daf49faf22965f44f4f
apoorv_13 <apoorvmishra13012007@gmail.com>
2026-02-20 21:39:01 +0530

    react-improovements2

--- maai/src/App.jsx
+++ maai/src/App.jsx
@@ -748,6 +748,9 @@ const ChatbotButton = ({ isVisible }) => {
         >
           <Motion.div
             layoutId="shared-mic"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0 }}
             transition={{ type: 'spring', stiffness: 300, damping: 30 }}
             whileHover={{ scale: 1.1, rotate: 10 }}
             whileTap={{ scale: 0.9 }}
